856e204365a155003e80725e4a0dc69578c008a8
hlushak <75501308+MarynaHl@users.noreply.github.com>
2022-07-14 12:26:50 +0100

    done

---
+++ js/task-02.js
@@ -0,0 +1,10 @@
+const magicBtn = document.querySelector('.js-magic-btn');
+
+magicBtn.addEventListener('click', () => {
+  const imageEl = document.querySelector('.hero__image');
+  console.log('imageEl', imageEl);
+  console.log(imageEl.src);
+  imageEl.src = 
+  'https://images.pexels.com/photos/57416/cat-sweet-kitty-animals-57416.jpeg?cs=srgb&dl=pexels-pixabay-57416.jpg&fm=jpg';
+  
+});
