52ade7424bcc35958ade6910781aab1fae5d1ec2
hlushak <75501308+MarynaHl@users.noreply.github.com>
2022-07-14 12:34:08 +0100

    done

--- js/task-02.js
+++ js/task-02.js
@@ -7,4 +7,9 @@ magicBtn.addEventListener('click', () => {
   imageEl.src = 
   'https://images.pexels.com/photos/57416/cat-sweet-kitty-animals-57416.jpeg?cs=srgb&dl=pexels-pixabay-57416.jpg&fm=jpg';
   imageEl.alt = 'New cat';
+
+  const heroTitleEl = document.querySelector('.hero__title');
+  console.log('heroTitleEl', heroTitleEl);
+  console.log(heroTitleEl.textContent);
+  heroTitleEl.textContent = 'I`m sweety'
 });
